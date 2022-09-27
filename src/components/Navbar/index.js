86df9db810518fdd2cb0e50ce00c3d3a24a11968
ob-shivam.goyal <shivam.goyal@onebanc.ai>
2022-09-27 23:09:55 +0530

    Your awesome message

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import React, { useState} from 'react';
+import { Nav, NavLink, NavIcon, Bars } from './NavbarElements';
+const Navbar = ({toggle })=> {
+    return (
+        <>
+            <Nav>
+                <NavLink to="/"> Pizzaa</NavLink>
+                <NavIcon onClick={toggle}>
+                    <p>Menu</p>
+                    <Bars/>
+                </NavIcon>
+            </Nav>
+        </>
+    );
+}
+
+export default Navbar;
